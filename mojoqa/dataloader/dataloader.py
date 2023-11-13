a0e0aed2e0085ec9e8bfb3cb662d8d06c3b2c133
Raigon Kunnath Augustin <raigonka@gmail.com>
2023-11-13 12:41:07 +0100

    changes to dataloader.py to create the dataset of Mojo documents

--- mojoqa/dataloader/dataloader.py
+++ mojoqa/dataloader/dataloader.py
@@ -1,9 +1,9 @@
-
-import os
+import pandas as pd
 import requests
-from urllib.parse import urljoin, urlparse
 from mojoqa.config.config import CFGLog
 from mojoqa.utils.config import Config
+from bs4 import BeautifulSoup
+from langchain.document_loaders import UnstructuredURLLoader
 
 
 class DataLoader:
@@ -20,26 +20,75 @@ class DataLoader:
         """
         self.data_config = data_config
 
-    def download_html_files_recursively(self, base_url, path):
-        response = requests.get(base_url)
-        os.makedirs(path, exist_ok=True)
-
+    @staticmethod
+    def get_html_page_data(url):
+        response = requests.get(url)
         if response.status_code == 200:
-            with open(os.path.join(path, "index.html"), "wb") as fp:
-                fp.write(response.content)
+            return response.text
+        else:
+            return -1
 
-        for link in response.text.split('href="')[1:]:
-            link = link.split('"', 1)[0]
-            if link.startswith(('http://', 'https://')):
-                new_url = link
+    def get_relevant_links(self, sub_page_page_url, base_url):
+        html_data = self.get_html_page_data(sub_page_page_url)
+        soup = BeautifulSoup(html_data, "html.parser")
+        list_links = []
+        dict_href_links = {}
+        for link in soup.find_all("a", href=True):
+
+            # Append to list if new link contains original link
+            if str(link["href"]).startswith((str(sub_page_page_url))):
+                list_links.append(link["href"])
+
+            # Include all href that do not start with website link but with "../"
+            if str(link["href"]).startswith("../"):
+                if link["href"] not in dict_href_links:
+                    print(link["href"])
+                    dict_href_links[link["href"]] = None
+                    link_with_www = base_url + link["href"][1:]
+                    print("adjusted link =", link_with_www)
+                    list_links.append(link_with_www)
+
+        # Convert list of links to dictionary and define keys as the links and the values as "Not-checked"
+        dict_links = dict.fromkeys(list_links, "Not-checked")
+        return dict_links
+
+    def get_sub_page_links(self, home_page_url, base_url) -> dict:
+        link_dict = {home_page_url: "Not-checked"}
+        for link in link_dict:
+            if link_dict[link] == "Not-checked":
+                dict_links_subpages = self.get_relevant_links(link, base_url)
+                link_dict[link] = "Checked"
             else:
-                new_url = urljoin(base_url, link)
+                dict_links_subpages = {}
 
-            if urlparse(new_url).netloc == urlparse(base_url).netloc:
-                self.download_html_files_recursively(new_url, os.path.join(path, urlparse(new_url).path.lstrip('/')))
+            link_dict = {**dict_links_subpages, **link_dict}
+
+        return link_dict
+
+    def create_mojo_docs_dataset(self, home_page_url: str, base_url: str):
+        relevant_links_dict = self.get_sub_page_links(home_page_url, base_url)
+
+        url_loader = UnstructuredURLLoader(urls=list(relevant_links_dict.keys()))
+        html_doc = url_loader.load()
+
+        html_page_data = [doc.page_content for doc in html_doc]
+        src = [doc.metadata['source'] for doc in html_doc]
+
+        mojo_docs_data = {
+            'id': list(range(len(html_page_data))),
+            'page_content': html_page_data,
+            'src': src
+        }
+
+        mojo_docs_data_frame = pd.DataFrame(mojo_docs_data)
+
+        return mojo_docs_data_frame
 
 
 if __name__ == "__main__":
     config = Config.from_json(CFGLog)
     data_obj = DataLoader(config.data)
-    data_obj.download_html_files_recursively(config.data.mojo_documentation_base_url, config.data.raw_html_files_path)
+    data_frame = data_obj.create_mojo_docs_dataset(
+        'https://docs.modular.com/mojo/', 'https://docs.modular.com/')
+    print(len(data_frame))
+    data_frame.to_csv('data.csv')
